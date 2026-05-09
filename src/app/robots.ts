f3aa0122264a82bdee3336e7a7c1b27baf33cdec
sabinayadhakal <sabinayadhakal007@gmail.com>
2026-05-09 19:06:18 +0545

    added sitemaps and robots text

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+// src/app/robots.ts
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://www.freetourkathmandu.com' // Change this to your actual domain
+  
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
